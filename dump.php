515f5b6c252612c25c1a00a25ec3ac0eee091df6
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-10-02 14:34:35 -0400

    Update dump.php

--- dump.php
+++ dump.php
@@ -5,8 +5,8 @@
     <body>
         <p> What is my User-Agent?
           <=? 
-          <form action="dump.php" method="POST"> 
-            $agent=$_GET['HTTP_User_Agent']; ?>
+         $_GET['HTTP_User_Agent']; 
+         ?>
     </p>
 </body>
     </html>
