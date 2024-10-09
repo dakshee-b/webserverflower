f8ac5d280bd470df4b1402e5c8567ccfcaa2a018
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-10-09 06:11:41 -0400

    updated toram.php

--- toram.php
+++ toram.php
@@ -1,16 +1,37 @@
 <! Doctype html> 
 <html>
     <head>
+      <title> Form response or answer </title>  
 </head>
     <body>
         <p>
-            <?=
-             var_dump ($_GET) 
+        What is your name?  <?=
+            htmlspecialchars ($_POST ['q1']) 
              ?>
         </p>
-        p>
-            <?=
-             var_dump ($_POST) 
+        <p>
+        How was your day? <?=
+            htmlspecialchars ($_POST ['q2']) 
+             ?>
+       </p>
+       <p>
+       How are you feeling right now?  <?=
+            htmlspecialchars ($_POST ['emotion']) 
+             ?>
+        </p> 
+        <p>
+        What is your favorite weather?  <?=
+            htmlspecialchars ($_POST ['weather']) 
+             ?>
+        </p>
+        <p>
+        What is your favorite moment during the day?  <?=
+            htmlspecialchars ($_POST ['24h']) 
+             ?>
+        </p>
+        <p>
+        How do you feel after answering the questions above?  <?=
+            htmlspecialchars ($_POST ['emotions']) 
              ?>
         </p>
 </body>
