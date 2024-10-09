bba41f377648c3de1150499d80074822f65a2b27
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-10-09 00:44:41 -0400

    Create toram.php

---
+++ toram.php
@@ -0,0 +1,17 @@
+<! Doctype html> 
+<html>
+    <head>
+</head>
+    <body>
+        <p>
+            <?=
+             var_dump ($_GET) 
+             ?>
+        </p>
+        p>
+            <?=
+             var_dump ($_POST) 
+             ?>
+        </p>
+</body>
+    </html>
