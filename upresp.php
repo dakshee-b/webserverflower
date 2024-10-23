66f5a4aa92bc8e2d6d4e24e9e21e49d10973eb86
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-10-22 22:17:37 -0400

    Update upresp.php

--- upresp.php
+++ upresp.php
@@ -13,10 +13,27 @@
           if (!conn) {
             die("Connection failed: {mysqli_connect_error()}");
           }
-          $sql = "select";
-          $result = ;
+          $sql = "CREATE TABLE visitors(
+          id int auto_increment primary key,
+          group_name varchar(255),
+          group_number varchar (255));";
+          $result = mysqli_query($conn, $sql);
           ?>
-</head>
+    </head>
     <body>
-</body>
-    </html>
+        <form action="upresp.php" method="post">
+            <label for="check-in">Select a group:</label></br>
+            <select id="group" name="group">
+                <?php
+                     foreach($result as $row){
+                        echo"<option value='{$row['group_number']}'> {$row['group_name']}</option></br>";
+                     }
+                     //close session (stop connection)
+                     mysqli_close($conn);
+                     ?>
+                     </select>
+                    </br>
+                    <input type="submit" value="submit"/>
+         </form>            
+    </body>
+</html>
