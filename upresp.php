a178216001945aeaa1b6ba9822542a8866d80384
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-10-23 01:43:02 -0400

    Update upresp.php

--- upresp.php
+++ upresp.php
@@ -19,9 +19,8 @@
     </head>
     <body>
                 <?php
-                     foreach($result as $row){
-                        echo"<option value='{$row['group_number']}'> {$row['group_name']}</option></br>";
-                     }
+                    foreach($result as $row){
+                        echo"<option value='{$row['group_number']}'> {$row['group_name']}</option></br>"; }
                      //close session (stop connection)
                      mysqli_close($conn);
                      ?>
