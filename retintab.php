51034bb101c09d82ff528eded6d204b68928a098
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-10-23 01:33:02 -0400

    Update retintab.php

--- retintab.php
+++ retintab.php
@@ -3,7 +3,7 @@
     <head>
       <title> Search info from table </title>  
       <?php 
-          $group_number = htmlspecialchars ($_POST ["find"]);
+          $group_number = htmlspecialchars ($_POST["find"]);
           $server = "localhost"; 
           $username = "ocl";
           $password = "0610jun96";
@@ -14,16 +14,21 @@
           if (!conn) {
             die("Connection failed: {mysqli_connect_error()}");
           }
-          $sql = "select group_name, group_number from visitors where group_name='{$group_name}';";
+          $sql = "select * from visitors where group_name='{$group_number}';";
           $result = mysqli_query($conn, $sql);
           ?>
     </head>
     <body>
-          You selected group:<?= $group_name; ?>. </br> 
+          You selected group:<?= htmlspecialchars ($group_number); ?>. </br> 
           <?php 
-          foreach($result as $row){
+          /*foreach($result as $row){
             echo"{$row['group_name']}' is the number {$row['group_name']}.<br>";
-         }
+         }*/
+         if(mysqli_num_rows($result) == 1 ) {
+          while($row = mysqli_fetch_assoc($result)){
+            echo $row['group_number'] is the number $row['group_name']. <br>;
+          }
+        }
          //close session (stop connection)
          mysqli_close($conn);
           ?>
