9021a868a3a5b96647e2c65e7b8a3d40462c5ed8
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-10-22 21:35:06 -0400

    Create upresp.php

---
+++ upresp.php
@@ -0,0 +1,22 @@
+<! Doctype html> 
+<html>
+    <head>
+      <title> Form response </title>  
+      <?php 
+          $server = "localhost"; 
+          $username = "ocl";
+          $password = "0610jun96";
+          $database = "Magic_Galaxy_Shop";
+          $conn = mysqli_connect ($server, $username, $password, $database);
+          
+          // Check for successful connection
+          if (!conn) {
+            die("Connection failed: {mysqli_connect_error()}");
+          }
+          $sql = "select";
+          $result = ;
+          ?>
+</head>
+    <body>
+</body>
+    </html>
