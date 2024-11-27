1ef969be8c1342f36a340032dc1c6d4d2677f79d
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-11-27 06:24:32 -0500

    Update rgpio.php

--- rgpio.php
+++ rgpio.php
@@ -23,25 +23,31 @@
         echo json_decode($raw, true); */
 
      
-          // Get the LED state from the form (either 'on' or 'off')
-          $led_state = htmlspecialchars ($_POST['led_state']);
+          
+          $temp = htmlspecialchars ($_POST['temp']);
+          $pres= htmlspecialchars ($_POST['pres']);
+          $alt = htmlspecialchars ($_POST['alt']);
       
-          // GPIO pin where the LED is connected (Example: GPIO pin 17)
-          $gpio_pin = 3;
+          
+          $raw = `./bme280`; 
+
+          echo $raw; 
+
+          $filter = json_decode($raw, true); 
+
+           var_dump($filter); 
+
       
-          // Ensure GPIO pin is configured for output (wiringPi or other library)
-          shell_exec("gpio mode $gpio_pin out");
-      
-          // Toggle the LED based on the form value
-          if ($led_state == 'on') {
-              // Turn the LED on (set the GPIO pin high)
-              shell_exec("gpio write $gpio_pin 1");
-              echo "<p>LED is ON</p>";
-          } elseif ($led_state == 'off') {
-              // Turn the LED off (set the GPIO pin low)
-              shell_exec("gpio write $gpio_pin 0");
-              echo "<p>LED is OFF</p>";
-          } 
+          
+          if ($temp == 'temperature') {
+              echo $filter["temperature"];
+
+          } elseif ($pres == 'pressure') {
+              echo $filter["pressure"];
+
+          } elseif ($alt == 'altitude') {
+            echo $filter["altitude"];
+        } 
 
              ?>
         </p>         
