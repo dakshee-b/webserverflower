c325fec275b60b235d2868ea5511239d33305728
dakshee-b <dakshee.boodadoo@dawsoncollege.qc.ca>
2024-11-27 05:54:53 -0500

    Update rgpio.php

--- rgpio.php
+++ rgpio.php
@@ -8,7 +8,7 @@
     <p>
          <?=
          /*  htmlspecialchars ($_POST ['data']); */
-          $raw = bme280;
+        /*  $raw = bme280;
            $deserialized = json_decode($raw, true);
            var_dump($deserialized);
            echo $deserialized["temperature"];
@@ -20,7 +20,31 @@
             "pressure" => $pressure,
             "altitude" => $altitude
         ); 
-        echo json_decode($raw, true);
+        echo json_decode($raw, true); */
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          // Get the LED state from the form (either 'on' or 'off')
+          $led_state = $_POST['led_state'];
+      
+          // GPIO pin where the LED is connected (Example: GPIO pin 17)
+          $gpio_pin = 3;
+      
+          // Ensure GPIO pin is configured for output (wiringPi or other library)
+          shell_exec("gpio mode $gpio_pin out");
+      
+          // Toggle the LED based on the form value
+          if ($led_state == 'on') {
+              // Turn the LED on (set the GPIO pin high)
+              shell_exec("gpio write $gpio_pin 1");
+              echo "<p>LED is ON</p>";
+          } elseif ($led_state == 'off') {
+              // Turn the LED off (set the GPIO pin low)
+              shell_exec("gpio write $gpio_pin 0");
+              echo "<p>LED is OFF</p>";
+          } else {
+              echo "<p>Invalid LED state.</p>";
+          }
+      }
              ?>
         </p>         
     </body>
